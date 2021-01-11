17a0796584cfe5104607926c72072537794cf104
vitalikkabai <vitalikkabai@gmail.com>
2021-01-11 11:05:15 +0200

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,7 @@ function App(): ReactElement {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi
     </div>
   );
 }
