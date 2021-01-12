1ed9654f636213c08d140ba66c817ec503ed03bb
vitalikkabai <vitalikkabai@gmail.com>
2021-01-12 18:34:13 +0200

    timer is done

--- src/components/DashBoard/Dashboard.tsx
+++ src/components/DashBoard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './Dashboard.module.scss';
 import logo from '../../assets/images/logo.svg';
 import number3 from '../../assets/images/number3.svg';
@@ -11,6 +11,25 @@ import clock from '../../assets/images/clock.svg';
 
 const Dashboard = () => {
 
+  //{ timeZone: 'Asia/Jerusalem' }) // this is Israel zone.
+  const israelTimeArr: string[] = new Date().toLocaleString('he-IL', { timeZone: 'Australia/Sydney' }).split(', ')[1].split(':');// change on Israel zone, Australia just from test
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [israelCurrentTime, setIsraelCurrentTime] = useState({ hour: parseInt(israelTimeArr[0]), min: parseInt(israelTimeArr[1]) });
+
+  useEffect(() => {
+    let prevTime = currentTime;
+    const intervalId = setInterval(() => {
+      const newTime = new Date();
+      const newIsraelTimeArr = new Date().toLocaleString('he-IL', { timeZone: 'Australia/Sydney' }).split(', ')[1].split(':');// change on Israel zone, Australia just from test
+      if (newTime.getMinutes() !== prevTime.getMinutes()) {
+        prevTime = newTime;
+        setIsraelCurrentTime({ hour: parseInt(newIsraelTimeArr[0]), min: parseInt(newIsraelTimeArr[1]) });
+        setCurrentTime(newTime);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
 
   return (
@@ -58,15 +77,36 @@ const Dashboard = () => {
           <Grid className={classes.clockContainer}>
             <Grid className={classes.clock}>
               <Typography>TIME IN ISRAEL</Typography>
-              <Typography className={classes.time}><img src={clock} alt="clock" /> 4:25</Typography>
+
+              <Typography className={classes.time}>
+                <img src={clock} alt="clock" />
+                {israelCurrentTime ?
+                  <Typography className={classes.time}>
+                    {israelCurrentTime.hour < 10 ? "0" + israelCurrentTime.hour : israelCurrentTime.hour}:
+                    {israelCurrentTime.min < 10 ? "0" + israelCurrentTime.min : israelCurrentTime.min}
+                  </Typography>
+                  : null
+                }</Typography>
             </Grid>
             <Grid className={classes.clock}>
               <Typography>TIME DIFFERENCE</Typography>
-              <Typography className={classes.time}><img src={clock} alt="clock" /> 1h</Typography>
+              <Typography className={classes.time}>
+                <img src={clock} alt="clock" />
+                {israelCurrentTime && currentTime ? currentTime.getHours() - israelCurrentTime.hour : null}
+              </Typography>
             </Grid>
             <Grid className={classes.clock}>
               <Typography>YOUR TIME</Typography>
-              <Typography className={classes.time}><img src={clock} alt="clock" /> 3:25</Typography>
+              <Typography className={classes.time}>
+                <img src={clock} alt="clock" />
+                {currentTime ?
+                  <Typography className={classes.time}>
+                    {currentTime.getHours() < 10 ? "0" + currentTime.getHours() : currentTime.getHours()}:
+                    {currentTime.getMinutes() < 10 ? "0" + currentTime.getMinutes() : currentTime.getMinutes()}
+                  </Typography>
+                  : null
+                }
+              </Typography>
             </Grid>
           </Grid>
 
