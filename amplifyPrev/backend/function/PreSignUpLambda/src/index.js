1bdf01f719e22102fd18cb6a36b0dacff1ce5aa7
livotron <livotron@gmail.com>
2021-01-19 14:37:21 +0200

    removed amplify prev

--- amplifyPrev/backend/function/PreSignUpLambda/src/index.js
+++
@@ -1,9 +0,0 @@
-
-
-exports.handler = (event, context, callback) => {
-    // Set the user pool autoConfirmUser flag after validating the email domain
-    event.response.autoConfirmUser = true;
-
-    // Return to Amazon Cognito
-    callback(null, event);
-};
