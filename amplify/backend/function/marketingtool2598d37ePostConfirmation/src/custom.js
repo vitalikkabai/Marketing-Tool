4ce8b457a8c9812f156ebef4e9c87c9b139dc80b
yevhen.smirnov <yevhen@parkingboxx.com>
2021-02-04 10:38:54 +0200

    added group assigning

--- amplify/backend/function/marketingtool2598d37ePostConfirmation/src/custom.js
+++ amplify/backend/function/marketingtool2598d37ePostConfirmation/src/custom.js
@@ -4,14 +4,29 @@ const AWS = require('aws-sdk');
 exports.handler = (event, context, callback) => {
   // insert code to be executed by your lambda trigger
   console.log(event);
-
+  let GroupName;
+  switch(event.request.userAttributes['custom:occupation']) {
+    case "0":
+      GroupName = "Employees";
+    break;
+    case '1':
+      GroupName = "Managers";
+    break;
+    case '2':
+      GroupName = "Freelancers";
+    break;
+    case '3':
+      GroupName = "Admins";
+    break;
+  }
   const params = {
-    GroupName: "Employees",
+    GroupName,
     UserPoolId: event.userPoolId,
     Username: event.userName
   };
-
-  AWS.CognitoIdentityServiceProvider.adminAddUserToGroup(params)
+  const cogintoIdentityService = new  AWS.CognitoIdentityServiceProvider();
+  console.log(cogintoIdentityService);
+  cogintoIdentityService.adminAddUserToGroup(params)
     .promise()
     .then(res => callback(null, event))
     .catch(err => callback(err, event));
