f793d27ab055c125575ceedfba094cee6883422f
Dovbaka <dovbaka99@gmail.com>
2021-02-09 15:36:14 +0200

    Created stepCounter and added steps to AddProduct

--- src/components/RegisterForm/UxAssistant/UxAssistant.tsx
+++ src/components/RegisterForm/UxAssistant/UxAssistant.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import classes from "../RegisterForm.module.scss";
 import {Box, Grid, Typography} from "@material-ui/core";
-import check from "../../../assets/images/checkMark.svg";
 import assistantAvatar from "../../../assets/images/avatar.png";
+import StepCounter from "../../common/StepCounter/StepCounter";
 
 interface PropTypes {
     stepNumber: number
@@ -12,21 +12,7 @@ interface PropTypes {
 const UxAssistant: React.FC<PropTypes> = (props) => {
     return (
         <>
-            <Grid item className={classes.progressContainer}>
-                <ul className={classes.progressCircles}>
-                    <li className={props.stepNumber === 1? classes.active + " " + classes.current + " " + classes.circleRipple : classes.active}>
-                        {props.stepNumber === 2 || props.stepNumber === 3? <img src={check} alt={"check"}/> : <Typography variant={"subtitle2"}>1</Typography>}
-                    </li>
-                    <div className={props.stepNumber === 2 || props.stepNumber === 3? classes.line + " " + classes.lineActive : classes.line}/>
-                    <li className={props.stepNumber === 2 ? classes.active + " " + classes.current + " " + classes.circleRipple : props.stepNumber === 1? "" : classes.active}>
-                        {props.stepNumber === 3? <img src={check} alt={"check"}/> : <Typography variant={"subtitle2"}>2</Typography>}
-                    </li>
-                    <div className={props.stepNumber === 3? classes.line + " " + classes.lineActive : classes.line}/>
-                    <li className={props.stepNumber === 3? classes.active + " " + classes.current + " " + classes.circleRipple : props.stepNumber === 2 || props.stepNumber === 1? "" : classes.active}>
-                        <Typography variant={"subtitle2"}>3</Typography>
-                    </li>
-                </ul>
-            </Grid>
+            <StepCounter stepNumber={props.stepNumber} stepColor={"66 133 244"} className={classes.stepCounter}/>
             <Grid item className={classes.assistantContainer}>
                 <img src={assistantAvatar} alt={"image"}/>
                 <Box className={classes.assistantText}>
