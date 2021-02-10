8a9b0726669bee463fe80cc5feac8937ab056b2a
vitalikkabai <vitalikkabai@gmail.com>
2021-02-10 14:43:55 +0200

    modify navBar

--- src/components/SideBarMenu/SideBarMenu.tsx
+++ src/components/SideBarMenu/SideBarMenu.tsx
@@ -18,17 +18,20 @@ const SideBarMenu = (props: any) => {
 	const theme = useTheme();
 	const history = useHistory();
 	const getCurrentLocation = () => {
-		if(history.location.pathname.includes("product")) return "2";
-		if(history.location.pathname.includes("market-research")) return "3";
-		if(history.location.pathname.includes("brand-creation")) return "4";
-		if(history.location.pathname.includes("sales-channels")) return "5";
-		if(history.location.pathname.includes("customer-support")) return "6";
-		if(history.location.pathname.includes("brand-awareness")) return "7";
-		if(history.location.pathname.includes("improvements")) return "8";
-		if(history.location.pathname.includes("sales-statistics")) return "9";
+		if (history.location.pathname.includes("product")) return "2";
+		if (history.location.pathname.includes("market-research")) return "3";
+		if (history.location.pathname.includes("brand-creation")) return "4";
+		if (history.location.pathname.includes("sales-channels")) return "5";
+		if (history.location.pathname.includes("customer-support")) return "6";
+		if (history.location.pathname.includes("brand-awareness")) return "7";
+		if (history.location.pathname.includes("improvements")) return "8";
+		if (history.location.pathname.includes("sales-statistics")) return "9";
 		return ("1")
 	}
+
 	const [selected, setSelected] = React.useState(getCurrentLocation());
+	const [isHover, setIsHover] = React.useState(false);
+	const [hovered, setHovered] = React.useState("");
 	return (
 		<Grid container direction={"column"} alignItems={"flex-start"} className={classes.menu}>
 			<Box className={classes.gridItem}>
@@ -42,6 +45,14 @@ const SideBarMenu = (props: any) => {
 				<ListItem button
 					selected={selected === "1"}
 					classes={{ root: classes.item1, selected: classes.active1 }}
+					onMouseEnter={() => {
+						setHovered("1")
+						setIsHover(!isHover)
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+						setIsHover(!isHover)
+					}}
 					onClick={
 						() => {
 							setSelected("1");
@@ -52,12 +63,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<CubeIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "1" ? "h6" : "subtitle1"}>Dashboard</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "1" || hovered === "1" ? "h6" : "subtitle1"}>Dashboard</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "2"}
 					classes={{ root: classes.item2, selected: classes.active2 }}
+					onMouseEnter={() => {
+						setHovered("2")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("2");
@@ -67,12 +84,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<BoxIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "2" ? "h6" : "subtitle1"}>Products</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "2" || hovered === "2" ? "h6" : "subtitle1"}>Products</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "3"}
 					classes={{ root: classes.item3, selected: classes.active3 }}
+					onMouseEnter={() => {
+						setHovered("3")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("3");
@@ -82,12 +105,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<ListIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "3" ? "h6" : "subtitle1"}>Market Research</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "3" || hovered === "3" ? "h6" : "subtitle1"}>Market Research</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "4"}
 					classes={{ root: classes.item4, selected: classes.active4 }}
+					onMouseEnter={() => {
+						setHovered("4")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("4");
@@ -97,12 +126,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<EditIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "4" ? "h6" : "subtitle1"}>Brand Creation</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "4" || hovered === "4" ? "h6" : "subtitle1"}>Brand Creation</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "5"}
 					classes={{ root: classes.item5, selected: classes.active5 }}
+					onMouseEnter={() => {
+						setHovered("5")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("5");
@@ -112,12 +147,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<SaleIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "5" ? "h6" : "subtitle1"}>Customer Support </Typography>} />
+					<ListItemText primary={<Typography variant={selected === "5" || hovered === "5" ? "h6" : "subtitle1"}>Sales Channels</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "6"}
 					classes={{ root: classes.item6, selected: classes.active6 }}
+					onMouseEnter={() => {
+						setHovered("6")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("6");
@@ -127,12 +168,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<SmileIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "6" ? "h6" : "subtitle1"}>Sales Channels</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "6" || hovered === "6" ? "h6" : "subtitle1"}>Customer Support</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "7"}
 					classes={{ root: classes.item7, selected: classes.active7 }}
+					onMouseEnter={() => {
+						setHovered("7")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("7");
@@ -142,12 +189,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<SpeakIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "7" ? "h6" : "subtitle1"}>Brand Awareness </Typography>} />
+					<ListItemText primary={<Typography variant={selected === "7" || hovered === "7" ? "h6" : "subtitle1"}>Brand Awareness </Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "8"}
 					classes={{ root: classes.item8, selected: classes.active8 }}
+					onMouseEnter={() => {
+						setHovered("8")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("8");
@@ -157,12 +210,18 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<GraphIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "8" ? "h6" : "subtitle1"}>Improvements</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "8" || hovered === "8" ? "h6" : "subtitle1"}>Sales Statistics</Typography>} />
 				</ListItem>
 
 				<ListItem button
 					selected={selected === "9"}
 					classes={{ root: classes.item9, selected: classes.active9 }}
+					onMouseEnter={() => {
+						setHovered("9")
+					}}
+					onMouseLeave={() => {
+						setHovered("")
+					}}
 					onClick={
 						() => {
 							setSelected("9");
@@ -172,14 +231,13 @@ const SideBarMenu = (props: any) => {
 					<ListItemIcon classes={{ root: classes.listIconRoot }}>
 						<StonksIcon className={classes.svgIcon} />
 					</ListItemIcon>
-					<ListItemText primary={<Typography variant={selected === "9" ? "h6" : "subtitle1"}>Sales</Typography>} />
+					<ListItemText primary={<Typography variant={selected === "9" || hovered === "9" ? "h6" : "subtitle1"}>Improvements</Typography>} />
 				</ListItem>
-
 			</List>
 			<Box className={classes.bottomText + " " + classes.gridItem}>
 				<hr style={{ border: "1px solid #F8F8F8", width: "236px", margin: "0 0 46px 0" }} />
 				<Box>
-					<Typography variant={"subtitle1"} color={"primary"} style={{cursor: "pointer"}}>+ Invite</Typography>
+					<Typography variant={"subtitle1"} color={"primary"} style={{ cursor: "pointer" }}>+ Invite</Typography>
 					<Typography variant={"subtitle1"}>Pricing</Typography>
 				</Box>
 			</Box>
