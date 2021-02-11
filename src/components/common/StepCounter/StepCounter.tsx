13b5eebc1fe3c073e8c69bb23665b8814d736762
vitalikkabai <vitalikkabai@gmail.com>
2021-02-11 14:52:03 +0200

    eight stepsCounter done

--- src/components/common/StepCounter/StepCounter.tsx
+++ src/components/common/StepCounter/StepCounter.tsx
@@ -1,55 +1,62 @@
-import classes from "./StepCounter.module.scss";
-import check from "../../../assets/images/checkMark.svg";
-import { Grid, makeStyles, Typography, useTheme } from "@material-ui/core";
-import React from "react";
+import classes from './StepCounter.module.scss';
+import check from '../../../assets/images/checkMark.svg';
+import { Grid, makeStyles, Typography, useTheme } from '@material-ui/core';
+import React from 'react';
 
 type PropsType = {
-    stepNumber: number
-    stepColor?: string
-    className?: string
-    completedStep?: number
-}
+    stepNumber: number;
+    stepColor?: string;
+    className?: string;
+    completedStep?: number;
+};
 
 const StepCounter: React.FC<PropsType> = (props) => {
+    const { stepNumber, stepColor, completedStep, className } = props;
 
     const getColor = () => {
-        if (props.completedStep === 1) return "248 208 0";
-        if (props.completedStep === 2) return "255 171 8";
-        if (props.completedStep === 3) return "238 107 29";
-        if (props.completedStep === 4) return "67 160 71";
-        if (props.completedStep === 5) return "0 151 166";
-        if (props.completedStep === 6) return "123 31 162";
-        if (props.completedStep === 7) return "218 75 123";
-        if (props.completedStep === 8) return "234 67 53";
-        return ("66 133 244")
-    }
+        if (completedStep === 1) return '248 208 0';
+        if (completedStep === 2) return '255 171 8';
+        if (completedStep === 3) return '238 107 29';
+        if (completedStep === 4) return '67 160 71';
+        if (completedStep === 5) return '0 151 166';
+        if (completedStep === 6) return '123 31 162';
+        if (completedStep === 7) return '218 75 123';
+        if (completedStep === 8) return '234 67 53';
+        return '66 133 244';
+    };
 
-    const getBackgroundColor = (stepNumberBackground?: number) => {
-        if (stepNumberBackground === 1) return "#F8D000";
-        if (stepNumberBackground === 2) return "#FFAB08";
-        if (stepNumberBackground === 3) return "#EE6B1D";
-        if (stepNumberBackground === 4) return "#43A047";
-        if (stepNumberBackground === 5) return "#0097A6";
-        if (stepNumberBackground === 6) return "#7B1FA2";
-        if (stepNumberBackground === 7) return "#DA4B7B";
-        if (stepNumberBackground === 8) return "#EA4335";
-        return ("#4285F4")
-    }
+    const getBackgroundColor = (stepNumberBackground: number) => {
+        if (stepNumberBackground === 1) return '#F8D000';
+        if (stepNumberBackground === 2) return '#FFAB08';
+        if (stepNumberBackground === 3) return '#EE6B1D';
+        if (stepNumberBackground === 4) return '#43A047';
+        if (stepNumberBackground === 5) return '#0097A6';
+        if (stepNumberBackground === 6) return '#7B1FA2';
+        if (stepNumberBackground === 7) return '#DA4B7B';
+        if (stepNumberBackground === 8) return '#EA4335';
+        return '#4285F4';
+    };
+
+    const stepsArr = [];
 
     const useStyles = makeStyles({
-        "@keyframes ripple": {
-            "0%": {
-                boxShadow: `0 0 0 0 rgb(${props.stepColor ? props.stepColor : getColor()}/ 60%)`
+        '@keyframes ripple': {
+            '0%': {
+                boxShadow: `0 0 0 0 rgb(${
+                    stepColor ? stepColor : getColor()
+                }/ 60%)`,
             },
-            "100% ": {
-                boxShadow: `0 0 0 0.6em rgb(${props.stepColor ? props.stepColor : getColor()} / 0%)`
-            }
+            '100% ': {
+                boxShadow: `0 0 0 0.6em rgb(${
+                    stepColor ? stepColor : getColor()
+                } / 0%)`,
+            },
         },
         circleRipple: {
-            backgroundColor: "#fff",
-            width: "40px",
-            height: "40px",
-            borderRadius: "50%",
+            backgroundColor: '#fff',
+            width: '40px',
+            height: '40px',
+            borderRadius: '50%',
             animation: `$ripple 1.7s linear infinite`,
         },
     });
@@ -57,120 +64,150 @@ const StepCounter: React.FC<PropsType> = (props) => {
 
     const theme = useTheme();
 
-    return <Grid item className={classes.stepProgressContainer + " " + props.className}>
-        {!props.completedStep &&
-            <ul className={classes.progressCircles}>
-                <li className={props.stepNumber === 1 ? classes.active + " " + classes.current + " " + styles.circleRipple : classes.active}
-                    style={{ background: theme.palette.primary.main }}>
-                    {props.stepNumber === 2 || props.stepNumber === 3 ? <img src={check} alt={"check"} /> :
-                        <Typography variant={"subtitle2"}>1</Typography>}
-                </li>
-                <div className={classes.line}
-                    style={props.stepNumber === 2 || props.stepNumber === 3 ? { background: theme.palette.primary.main } : {}} />
-                <li className={props.stepNumber === 2 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 1 ? "" : classes.active}
-                    style={props.stepNumber === 2 ? { background: theme.palette.primary.main } : props.stepNumber === 1 ? {} : { background: theme.palette.primary.main }}>
-                    {props.stepNumber === 3 ? <img src={check} alt={"check"} /> :
-                        <Typography variant={"subtitle2"}>2</Typography>}
-                </li>
-                <div className={classes.line}
-                    style={props.stepNumber === 3 ? { background: theme.palette.primary.main } : {}} />
-                <li className={props.stepNumber === 3 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 2 || props.stepNumber === 1 ? "" : classes.active}
-                    style={props.stepNumber === 3 ? { background: theme.palette.primary.main } : props.stepNumber === 2 || props.stepNumber === 1 ? {} : { background: theme.palette.primary.main }}>
-                    <Typography variant={"subtitle2"}>3</Typography>
-                </li>
-            </ul>
+    if (completedStep) {
+        for (let i = 0; i <= 8; i++) {
+            stepsArr.push(
+                <>
+                    <li
+                        key={`li${i}`}
+                        className={
+                            completedStep === i
+                                ? classes.active +
+                                  ' ' +
+                                  classes.current +
+                                  ' ' +
+                                  styles.circleRipple
+                                : stepNumber === i && stepNumber > completedStep
+                                ? classes.current
+                                : ' '
+                        }
+                        style={
+                            completedStep >= i
+                                ? { background: getBackgroundColor(i) }
+                                : stepNumber === i
+                                ? {
+                                      border: `3px solid ${getBackgroundColor(
+                                          i
+                                      )}`,
+                                  }
+                                : {}
+                        }
+                    >
+                        {completedStep > i ? (
+                            <img src={check} alt={'check'} />
+                        ) : (
+                            <Typography variant={'subtitle2'}>{i}</Typography>
+                        )}
+                    </li>
+
+                    {i < 8 ? (
+                        <div
+                            key={`div${i}`}
+                            className={classes.line}
+                            style={
+                                completedStep >= i + 1
+                                    ? { background: getBackgroundColor(i) }
+                                    : {}
+                            }
+                        />
+                    ) : null}
+                </>
+            );
         }
+    }
 
-        {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-
-        {props.completedStep &&
-            <>
+    return (
+        <Grid item className={classes.stepProgressContainer + ' ' + className}>
+            {!completedStep && (
                 <ul className={classes.progressCircles}>
-                    <li className={props.completedStep === 0 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 0 ? classes.current : " "}
-                        style={{ background: getBackgroundColor(0) }}>
-                        <img src={check} alt={"check"} />
+                    <li
+                        className={
+                            stepNumber === 1
+                                ? classes.active +
+                                  ' ' +
+                                  classes.current +
+                                  ' ' +
+                                  styles.circleRipple
+                                : classes.active
+                        }
+                        style={{ background: theme.palette.primary.main }}
+                    >
+                        {stepNumber === 2 || stepNumber === 3 ? (
+                            <img src={check} alt={'check'} />
+                        ) : (
+                            <Typography variant={'subtitle2'}>1</Typography>
+                        )}
                     </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 1 ? { background: getBackgroundColor(0) } : {}} />
-
-                    <li className={props.completedStep === 1 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 1 ? classes.current : " "}
-                        style={props.completedStep >= 1 ? { background: getBackgroundColor(1) } : props.stepNumber === 1 ? { border: `3px solid ${getBackgroundColor(1)}` } : {}}>
-                        {props.completedStep > 1 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>1</Typography>}
+                    <div
+                        className={classes.line}
+                        style={
+                            stepNumber === 2 || stepNumber === 3
+                                ? { background: theme.palette.primary.main }
+                                : {}
+                        }
+                    />
+                    <li
+                        className={
+                            stepNumber === 2
+                                ? classes.active +
+                                  ' ' +
+                                  classes.current +
+                                  ' ' +
+                                  styles.circleRipple
+                                : stepNumber === 1
+                                ? ''
+                                : classes.active
+                        }
+                        style={
+                            stepNumber === 2
+                                ? { background: theme.palette.primary.main }
+                                : stepNumber === 1
+                                ? {}
+                                : { background: theme.palette.primary.main }
+                        }
+                    >
+                        {stepNumber === 3 ? (
+                            <img src={check} alt={'check'} />
+                        ) : (
+                            <Typography variant={'subtitle2'}>2</Typography>
+                        )}
                     </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 2 ? { background: getBackgroundColor(1) } : {}} />
-
-                    <li className={props.completedStep === 2 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 2 ? classes.current : " "}
-                        style={props.completedStep >= 2 ? { background: getBackgroundColor(2) } : props.stepNumber === 2 ? { border: `3px solid ${getBackgroundColor(2)}` } : {}}>
-                        {props.completedStep > 2 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>2</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 3 ? { background: getBackgroundColor(2) } : {}} />
-
-                    <li className={props.completedStep === 3 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 3 ? classes.current : " "}
-                        style={props.completedStep >= 3 ? { background: getBackgroundColor(3) } : props.stepNumber === 3 ? { border: `3px solid ${getBackgroundColor(3)}` } : {}}>
-                        {props.completedStep > 3 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>3</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 4 ? { background: getBackgroundColor(3) } : {}} />
-
-                    <li className={props.completedStep === 4 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 4 ? classes.current : " "}
-                        style={props.completedStep >= 4 ? { background: getBackgroundColor(4) } : props.stepNumber === 4 ? { border: `3px solid ${getBackgroundColor(4)}` } : {}}>
-                        {props.completedStep > 4 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>4</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 5 ? { background: getBackgroundColor(4) } : {}} />
-
-                    <li className={props.completedStep === 5 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 5 ? classes.current : " "}
-                        style={props.completedStep >= 5 ? { background: getBackgroundColor(5) } : props.stepNumber === 5 ? { border: `3px solid ${getBackgroundColor(5)}` } : {}}>
-                        {props.completedStep > 5 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>5</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 6 ? { background: getBackgroundColor(5) } : {}} />
-
-                    <li className={props.completedStep === 6 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 6 ? classes.current : " "}
-                        style={props.completedStep >= 6 ? { background: getBackgroundColor(6) } : props.stepNumber === 6 ? { border: `3px solid ${getBackgroundColor(6)}` } : {}}>
-                        {props.completedStep > 6 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>6</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 7 ? { background: getBackgroundColor(6) } : {}} />
-
-                    <li className={props.completedStep === 7 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 7 ? classes.current : " "}
-                        style={props.completedStep >= 7 ? { background: getBackgroundColor(7) } : props.stepNumber === 7 ? { border: `3px solid ${getBackgroundColor(7)}` } : {}}>
-                        {props.completedStep > 7 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>7</Typography>}
-                    </li>
-
-                    <div className={classes.line}
-                        style={props.completedStep >= 8 ? { background: getBackgroundColor(7) } : {}} />
-
-                    <li className={props.completedStep === 8 ? classes.active + " " + classes.current + " " + styles.circleRipple : props.stepNumber === 8 ? classes.current : " "}
-                        style={props.completedStep >= 8 ? { background: getBackgroundColor(8) } : props.stepNumber === 8 ? { border: `3px solid ${getBackgroundColor(8)}` } : {}}>
-                        {props.completedStep > 8 ? <img src={check} alt={"check"} /> :
-                            <Typography variant={"subtitle2"}>8</Typography>}
+                    <div
+                        className={classes.line}
+                        style={
+                            stepNumber === 3
+                                ? { background: theme.palette.primary.main }
+                                : {}
+                        }
+                    />
+                    <li
+                        className={
+                            stepNumber === 3
+                                ? classes.active +
+                                  ' ' +
+                                  classes.current +
+                                  ' ' +
+                                  styles.circleRipple
+                                : stepNumber === 2 || stepNumber === 1
+                                ? ''
+                                : classes.active
+                        }
+                        style={
+                            stepNumber === 3
+                                ? { background: theme.palette.primary.main }
+                                : stepNumber === 2 || stepNumber === 1
+                                ? {}
+                                : { background: theme.palette.primary.main }
+                        }
+                    >
+                        <Typography variant={'subtitle2'}>3</Typography>
                     </li>
                 </ul>
-                <ul>
+            )}
 
-                </ul>
-            </>
-        }
-
-
-    </Grid >
-}
+            {completedStep && <ul>{stepsArr}</ul>}
+        </Grid>
+    );
+};
 
 export default StepCounter;
--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,73 +1,136 @@
-import React from "react";
-import { PropsFromRedux } from "./ProductPageContainer";
-import { Box, Grid, Tab, Typography } from "@material-ui/core";
-import classes from "./ProductPage.module.scss";
-import CustomAppBar from "../../components/common/TabPanel/CustomAppBar";
-import TabPanel from "../../components/common/TabPanel/TabPanel";
-import Blob from "../../assets/images/Blob.png";
-import { ReactComponent as PlusSVG } from "../../assets/images/formPlus.svg";
-import { useHistory } from "react-router";
+import React from 'react';
+import { PropsFromRedux } from './ProductPageContainer';
+import { Box, Grid, Tab, Typography } from '@material-ui/core';
+import classes from './ProductPage.module.scss';
+import CustomAppBar from '../../components/common/TabPanel/CustomAppBar';
+import TabPanel from '../../components/common/TabPanel/TabPanel';
+import ProductCat from '../../assets/images/ProductCat.png';
+import { ReactComponent as PlusSVG } from '../../assets/images/formPlus.svg';
+import { useHistory } from 'react-router';
 import StepCounter from './../../components/common/StepCounter/StepCounter';
 
 const ProductPage: React.FunctionComponent<PropsFromRedux> = (props) => {
+    const [value, setValue] = React.useState(0);
+    const history = useHistory();
 
-	const [value, setValue] = React.useState(0);
-	const history = useHistory();
+    return (
+        <Grid container className={classes.dashboard}>
+            <Box className={classes.todoTitleBox}>
+                <Grid item className={classes.titleBox}>
+                    <Typography variant={'h2'}>Products</Typography>
+                </Grid>
+                <Grid
+                    item
+                    className={classes.addProductButton}
+                    onClick={() => {
+                        history.push('/products/add-new-product');
+                    }}
+                >
+                    <PlusSVG />
+                    <Typography color={'primary'} variant={'button'}>
+                        Add product
+                    </Typography>
+                </Grid>
+            </Box>
+            <Box className={classes.contentContainer}>
+                <Grid item className={classes.contentBox} container xs={12}>
+                    <CustomAppBar value={value} setValue={setValue}>
+                        <Tab label="Added" color={'primary'} />
+                        <Tab label="Edited" color={'primary'} />
+                        <Tab label="Sales" color={'primary'} />
+                    </CustomAppBar>
 
-	return <Grid container className={classes.dashboard}>
-		<Box className={classes.todoTitleBox}>
-			<Grid item className={classes.titleBox}>
-				<Typography variant={"h2"}>Products</Typography>
-			</Grid>
-			<Grid item className={classes.addProductButton} onClick={() => { history.push("/products/add-new-product") }}>
-				<PlusSVG />
-				<Typography color={"primary"} variant={"button"}>Add product</Typography>
-			</Grid>
-		</Box>
-		<Box className={classes.contentContainer}>
-			<Grid item className={classes.contentBox} container xs={12}>
-				<CustomAppBar value={value} setValue={setValue}>
-					<Tab label="Added" color={"primary"} />
-					<Tab label="Edited" color={"primary"} />
-					<Tab label="Sales" color={"primary"} />
-				</CustomAppBar>
+                    <TabPanel
+                        className={classes.tabPanelBox}
+                        index={0}
+                        value={value}
+                    >
+                        <Grid item xs={12} className={classes.taskContainer}>
+                            <Box
+                                marginTop={'57px'}
+                                textAlign={'center'}
+                                className={classes.tasksTitle}
+                            >
+                                <Typography
+                                    variant={'h2'}
+                                    style={{ textTransform: 'uppercase' }}
+                                >
+                                    Complete this step
+                                </Typography>
+                            </Box>
+                            <Box
+                                textAlign={'center'}
+                                justifyContent={'center'}
+                                alignItems={'center'}
+                            >
+                                <StepCounter
+                                    className={classes.stepLine}
+                                    completedStep={1}
+                                    stepNumber={1}
+                                />
+                                <img src={ProductCat} alt={'image'} />
+                            </Box>
+                        </Grid>
+                    </TabPanel>
 
-				<TabPanel className={classes.tabPanelBox} index={0} value={value}>
-					<Grid item xs={12} className={classes.taskContainer}>
-						<Box marginTop={"57px"} textAlign={"center"} className={classes.tasksTitle}>
-							<Typography variant={"body2"} style={{ textTransform: "uppercase" }}>Complete this step</Typography>
-						</Box>
-						<Box textAlign={"center"} justifyContent={"center"} alignItems={"center"} >
-							<StepCounter completedStep={3} stepNumber={7} />
-							<img src={Blob} alt={"image"} />
-						</Box>
-					</Grid>
-				</TabPanel>
-
-				<TabPanel className={classes.tabPanelBox} index={1} value={value}>
-					<Grid item xs={12} className={classes.taskContainer}>
-						<Box marginTop={"57px"} textAlign={"center"} className={classes.tasksTitle}>
-							<Typography variant={"body2"} style={{ textTransform: "uppercase" }}>This page is not ready yet</Typography>
-						</Box>
-						<Box textAlign={"center"} justifyContent={"center"} alignItems={"center"} >
-							<img src={Blob} alt={"image"} />
-						</Box>
-					</Grid>
-				</TabPanel>
-				<TabPanel className={classes.tabPanelBox} index={2} value={value}>
-					<Grid item xs={12} className={classes.taskContainer}>
-						<Box marginTop={"57px"} textAlign={"center"} className={classes.tasksTitle}>
-							<Typography variant={"body2"} style={{ textTransform: "uppercase" }}>This page is not ready yet</Typography>
-						</Box>
-						<Box textAlign={"center"} justifyContent={"center"} alignItems={"center"} >
-							<img src={Blob} alt={"image"} />
-						</Box>
-					</Grid>
-				</TabPanel>
-			</Grid>
-		</Box>
-
-	</Grid>
-}
+                    <TabPanel
+                        className={classes.tabPanelBox}
+                        index={1}
+                        value={value}
+                    >
+                        <Grid item xs={12} className={classes.taskContainer}>
+                            <Box
+                                marginTop={'57px'}
+                                textAlign={'center'}
+                                className={classes.tasksTitle}
+                            >
+                                <Typography
+                                    variant={'body2'}
+                                    style={{ textTransform: 'uppercase' }}
+                                >
+                                    This page is not ready yet
+                                </Typography>
+                            </Box>
+                            <Box
+                                textAlign={'center'}
+                                justifyContent={'center'}
+                                alignItems={'center'}
+                            >
+                                <img src={ProductCat} alt={'image'} />
+                            </Box>
+                        </Grid>
+                    </TabPanel>
+                    <TabPanel
+                        className={classes.tabPanelBox}
+                        index={2}
+                        value={value}
+                    >
+                        <Grid item xs={12} className={classes.taskContainer}>
+                            <Box
+                                marginTop={'57px'}
+                                textAlign={'center'}
+                                className={classes.tasksTitle}
+                            >
+                                <Typography
+                                    variant={'body2'}
+                                    style={{ textTransform: 'uppercase' }}
+                                >
+                                    This page is not ready yet
+                                </Typography>
+                            </Box>
+                            <Box
+                                textAlign={'center'}
+                                justifyContent={'center'}
+                                alignItems={'center'}
+                            >
+                                <img src={ProductCat} alt={'image'} />
+                            </Box>
+                        </Grid>
+                    </TabPanel>
+                </Grid>
+            </Box>
+        </Grid>
+    );
+};
 
 export default ProductPage;
