feeb019303c9bdabcd06dbbb9f993b85ff775012
vitalikkabai <vitalikkabai@gmail.com>
2021-02-16 12:45:24 +0200

    add delete button on dropzone carousel

--- src/components/common/Carousel/CustomCarousel.tsx
+++ src/components/common/Carousel/CustomCarousel.tsx
@@ -1,51 +1,72 @@
-import React, {useEffect, useRef} from "react";
-import classes from "./Carousel.module.scss"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import {ReactComponent as NextArrow} from "../../../assets/images/arrowRight.svg"
-import {ReactComponent as PrewArrow} from "../../../assets/images/arrowLeft.svg"
+import React, { useEffect, useRef } from 'react';
+import classes from './Carousel.module.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { ReactComponent as NextArrow } from '../../../assets/images/arrowRight.svg';
+import { ReactComponent as PrewArrow } from '../../../assets/images/arrowLeft.svg';
 
-import Slider from "react-slick";
-import Box from "@material-ui/core/Box";
-
+import Slider from 'react-slick';
+import Box from '@material-ui/core/Box';
 
 type PropsType = {
-    Items: any
-    className?: string
-}
+    Items: any;
+    className?: string;
+};
 
-
-const CustomCarousel: React.FunctionComponent<PropsType> = ({Items, className}) => {
-
+const CustomCarousel: React.FunctionComponent<PropsType> = ({
+    Items,
+    className,
+}) => {
     let slider = useRef() as any;
 
     const next = () => {
         slider.slickNext();
-    }
+    };
     const previous = () => {
         slider.slickPrev();
-    }
+    };
 
-    useEffect(()=>{
-        if(Items.length > 2) {
+    useEffect(() => {
+        if (Items.length > 2) {
             slider.slickNext();
         }
-    },[Items.length]);
+    }, [Items.length]);
+
+    //ToDo fix slick next event on items degrees
 
     return (
-        <div className={classes.sliderContainer + " " + className}>
-            <Box onClick={previous} className={classes.sliderArrow} style={Items.length === 0 || Items.length < 3? {display: "none"}:{}}>
-                <PrewArrow/>
+        <div className={classes.sliderContainer + ' ' + className}>
+            <Box
+                onClick={previous}
+                className={classes.sliderArrow}
+                style={
+                    Items.length === 0 || Items.length < 3
+                        ? { display: 'none' }
+                        : {}
+                }>
+                <PrewArrow />
             </Box>
-            <Slider dots={false} infinite={false} slidesToShow={2}
-                    className={classes.carouselBox} arrows={false}
-                    ref={c => (slider = c)}>{Items.map((item: any) => item)}</Slider>
-            <Box onClick={next} className={classes.sliderArrow} style={Items.length === 0 || Items.length < 3? {display: "none"}:{}}>
-                <NextArrow/>
+            <Slider
+                dots={false}
+                infinite={false}
+                slidesToShow={2}
+                className={classes.carouselBox}
+                arrows={false}
+                ref={(c) => (slider = c)}>
+                {Items.map((item: any) => item)}
+            </Slider>
+            <Box
+                onClick={next}
+                className={classes.sliderArrow}
+                style={
+                    Items.length === 0 || Items.length < 3
+                        ? { display: 'none' }
+                        : {}
+                }>
+                <NextArrow />
             </Box>
         </div>
-
     );
-}
+};
 
 export default CustomCarousel;
