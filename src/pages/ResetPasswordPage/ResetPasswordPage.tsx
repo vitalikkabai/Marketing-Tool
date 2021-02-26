ca4eef2535f9b66e040b65aa0c69e8e01f98f66e
Dovbaka <dovbaka99@gmail.com>
2021-02-26 11:43:31 +0200

    Deleted commented code

--- src/pages/ResetPasswordPage/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -4,90 +4,10 @@ import SendResetLink from '../../components/ResetPasswordForm/SendResetLink';
 import ResetPasswordForm from '../../components/ResetPasswordForm/ResetPasswordForm';
 import ResetIsDone from '../../components/ResetPasswordForm/ResetsIsDone';
 import { PropsFromRedux } from './ResetPasswordPageContainer';
-import {useFormik} from "formik";
-import * as Yup from "yup";
 
 const ResetPasswordPage: React.FC<PropsFromRedux> = (props) => {
     const [isEmailEntered, setEmailEntered] = useState(false);
     const [isNewPasswordEntered, setIsNewPasswordEntered] = useState(false);
-
-    /*const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        props.cleanErrors();
-
-        if (!isNewPasswordEntered && !isEmailEntered) {
-            if (inputValue.email.value && !emailError[0]) {
-                props.sendEmail(inputValue.email.value);
-            } else {
-                if (emailError[0])
-                    setInputValue((prevStyle) => ({
-                        ...prevStyle,
-                        email: {
-                            ...prevStyle.email,
-                            error: true,
-                            errorText: emailError[0],
-                        },
-                    }));
-            }
-        }
-        if (isEmailEntered) {
-            if (codeError[0])
-                setInputValue((prevStyle) => ({
-                    ...prevStyle,
-                    code: {
-                        ...prevStyle.code,
-                        error: true,
-                        errorText: codeError[0],
-                    },
-                }));
-            if (newPasswordError[0])
-                setInputValue((prevStyle) => ({
-                    ...prevStyle,
-                    newPassword: {
-                        ...prevStyle.newPassword,
-                        error: true,
-                        errorText: newPasswordError[0],
-                    },
-                }));
-            if (retypeError[0])
-                setInputValue((prevStyle) => ({
-                    ...prevStyle,
-                    retypePassword: {
-                        ...prevStyle.retypePassword,
-                        error: true,
-                        errorText: retypeError[0],
-                    },
-                }));
-            if (!codeError[0] && !newPasswordError[0] && !retypeError[0]) {
-                if (
-                    !isPasswordsNotEqual(
-                        inputValue.newPassword.value,
-                        inputValue.retypePassword.value
-                    )
-                ) {
-                    props.sendNewPassword(
-                        inputValue.email.value,
-                        inputValue.code.value,
-                        inputValue.newPassword.value
-                    );
-                } else {
-                    setInputValue((prevStyle) => ({
-                        ...prevStyle,
-                        newPassword: {
-                            ...prevStyle.newPassword,
-                            error: true,
-                            errorText: 'Password missmached',
-                        },
-                        retypePassword: {
-                            ...prevStyle.retypePassword,
-                            error: true,
-                        },
-                    }));
-                }
-            }
-        }
-    };*/
-
 
     useEffect(() => {
         setEmailEntered(props.isSendResetLinkSuccess);
