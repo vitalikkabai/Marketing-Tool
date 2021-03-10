66f210659ceb7a29acae31ac73feb6af067366e1
andrii-kuskovets <50615736+andrii-kuskovets@users.noreply.github.com>
2021-03-10 11:17:51 +0200

    Fix Header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import classes from './Header.module.scss';
-import headerLogo from '../../assets/images/headerLogo.svg';
-import headerLogoForMobile from '../../assets/images/logoMob.svg';
+import {ReactComponent as HeaderLogo} from '../../assets/images/headerLogo.svg';
+import {ReactComponent as HeaderLogoForMobile} from '../../assets/images/logoMob.svg';
 import { useHistory } from 'react-router';
 
 const Header: React.FC<Record<string, unknown>> = () => {
     const history = useHistory();
     return (
-        <div className={classes.headerContainer}>
-            <img
+        <Box className={classes.headerContainer}>
+            <HeaderLogo 
                 className={classes.headerContainerImgDesktop}
-                src={headerLogo}
-                alt={'image'}
                 style={{ cursor: 'pointer' }}
                 onClick={() => history.replace('/login')}
             />
-            <img
+            <HeaderLogoForMobile
                 className={classes.headerContainerImgMobile}
-                src={headerLogoForMobile}
-                alt={'image'}
                 style={{ cursor: 'pointer' }}
                 onClick={() => history.replace('/login')}
             />
-        </div>
+        </Box>
     );
 };
 
